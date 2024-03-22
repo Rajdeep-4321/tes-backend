ff3b9f7a9e9792aaa094f2b1d4ff5b6d63639d6f
Rajdeep Shah <rajdeepshah@Rajdeeps-MacBook-Pro.local>
2024-03-23 01:10:48 +0530

    make it better

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "cluster0.lkrezic.mongodb.net/",
+  PORT: 27017,
+  DB: "TES",
+  mongoUrl: "mongodb+srv://mongo_rajdeep:jXYRf70mKOZR0EVy@cluster0.lkrezic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+}
