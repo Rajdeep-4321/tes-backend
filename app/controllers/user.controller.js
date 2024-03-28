4d0b4b68f7e12f69c347ada4a2a2425a35c07c87
Rajdeep Shah <rajdeepshah@Rajdeeps-MacBook-Pro.local>
2024-03-29 00:47:10 +0530

    Initial deployment to Heroku

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,3 +1,9 @@
+const axios = require("axios");
+const https = require('https');
+
+const agent = new https.Agent({
+  keepAlive: true,
+});
 exports.allAccess = (req, res) => {
   res.status(200).send("Public Content.");
 };
@@ -8,6 +14,31 @@ exports.userBoard = (req, res) => {
 
 exports.adminBoard = (req, res) => {
   res.status(200).send("Admin Content.");
+};
+
+exports.getStockData = (req, res) => {
+  const secretKey = 'MSrIDco7b2pAUe3t39WZgax110d2kVefKD7dhbuc';
+  const apiKey = 'CKP3MQZGSRKBGC1LP688';
+
+  const username = 'CKP3MQZGSRKBGC1LP688';
+  const password = 'MSrIDco7b2pAUe3t39WZgax110d2kVefKD7dhbuc';
+  
+  // Encoding username and password in base64
+  const basicAuthCredentials = Buffer.from(`${username}:${password}`, 'utf-8').toString('base64');
+  const headers = {
+
+    'Authorization': `Basic ${basicAuthCredentials}`
+
+  };
+  axios.get('https://broker-api.sandbox.alpaca.markets/v1/assets', { headers })
+  .then(response => {    
+    return res.status(200).send(response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching stock data:', error);
+    return res.status(500).send('Error fetching stock data');
+  });
+  // res.status(200).send("Admin Content.");
 };
 
 exports.moderatorBoard = (req, res) => {
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -25,4 +25,9 @@ module.exports = function(app) {
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
+
+  app.get(
+    "/api/get-stock-data",
+    controller.getStockData
+  );
 };
